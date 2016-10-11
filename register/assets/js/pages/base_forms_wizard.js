9c33fe520ae51c1711cba645f09dabeb829cd742
rishanthakumarrasarathinam <rishanthakumar@gmail.com>
2016-10-11 09:08:10 +0530

    Added google map auto complete and configured jquery validation rules.

--- register/assets/js/pages/base_forms_wizard.js
+++ register/assets/js/pages/base_forms_wizard.js
@@ -5,192 +5,6 @@
  */
 
 var BaseFormWizard = function() {
-    // Init simple wizard, for more examples you can check out http://vadimg.com/twitter-bootstrap-wizard-example/
-    var initWizardSimple = function(){
-
-
-        // Get forms
-        var $form1 = jQuery('.js-form1');
-        var $form2 = jQuery('.js-form2');
-
-        // Prevent forms from submitting on enter key press
-        $form1.add($form2).on('keyup keypress', function (e) {
-            var code = e.keyCode || e.which;
-
-            if (code === 13) {
-                e.preventDefault();
-                return false;
-            }
-        });
-
-        // Init form validation on classic wizard form
-        var $validator1 = $form1.validate({
-            errorClass: 'help-block animated fadeInDown',
-            errorElement: 'div',
-            errorPlacement: function(error, e) {
-                jQuery(e).parents('.form-group > div').append(error);
-            },
-            highlight: function(e) {
-                jQuery(e).closest('.form-group').removeClass('has-error').addClass('has-error');
-                jQuery(e).closest('.help-block').remove();
-            },
-            success: function(e) {
-                jQuery(e).closest('.form-group').removeClass('has-error');
-                jQuery(e).closest('.help-block').remove();
-            },
-            rules: {
-                'validation-classic-firstname': {
-                    required: true,
-                    minlength: 2
-                },
-                'validation-classic-lastname': {
-                    required: true,
-                    minlength: 2
-                },
-                'validation-classic-email': {
-                    required: true,
-                    email: true
-                },
-                'validation-classic-details': {
-                    required: true,
-                    minlength: 5
-                },
-                'validation-classic-city': {
-                    required: true
-                },
-                'validation-classic-skills': {
-                    required: true
-                },
-                'validation-classic-terms': {
-                    required: true
-                }
-            },
-            messages: {
-                'validation-classic-firstname': {
-                    required: 'Please enter a firstname',
-                    minlength: 'Your firtname must consist of at least 2 characters'
-                },
-                'validation-classic-lastname': {
-                    required: 'Please enter a lastname',
-                    minlength: 'Your lastname must consist of at least 2 characters'
-                },
-                'validation-classic-email': 'Please enter a valid email address',
-                'validation-classic-details': 'Let us know a few thing about yourself',
-                'validation-classic-skills': 'Please select a skill!',
-                'validation-classic-terms': 'You must agree to the service terms!'
-            }
-        });
-
-        // Init form validation on the other wizard form
-        var $validator2 = $form2.validate({
-            errorClass: 'help-block text-right animated fadeInDown',
-            errorElement: 'div',
-            errorPlacement: function(error, e) {
-                jQuery(e).parents('.form-group .form-material').append(error);
-            },
-            highlight: function(e) {
-                jQuery(e).closest('.form-group').removeClass('has-error').addClass('has-error');
-                jQuery(e).closest('.help-block').remove();
-            },
-            success: function(e) {
-                jQuery(e).closest('.form-group').removeClass('has-error');
-                jQuery(e).closest('.help-block').remove();
-            },
-            rules: {
-                'validation-firstname': {
-                    required: true,
-                    minlength: 2
-                },
-                'validation-lastname': {
-                    required: true,
-                    minlength: 2
-                },
-                'validation-email': {
-                    required: true,
-                    email: true
-                },
-                'validation-details': {
-                    required: true,
-                    minlength: 5
-                },
-                'validation-city': {
-                    required: true
-                },
-                'validation-skills': {
-                    required: true
-                },
-                'validation-terms': {
-                    required: true
-                }
-            },
-            messages: {
-                'validation-firstname': {
-                    required: 'Please enter a firstname',
-                    minlength: 'Your firtname must consist of at least 2 characters'
-                },
-                'validation-lastname': {
-                    required: 'Please enter a lastname',
-                    minlength: 'Your lastname must consist of at least 2 characters'
-                },
-                'validation-email': 'Please enter a valid email address',
-                'validation-details': 'Let us know a few thing about yourself',
-                'validation-skills': 'Please select a skill!',
-                'validation-terms': 'You must agree to the service terms!'
-            }
-        });
-
-
-
-        jQuery('.js-wizard-simple').bootstrapWizard({
-            'tabClass': '',
-            'firstSelector': '.wizard-first',
-            'previousSelector': '.wizard-prev',
-            'nextSelector': '.wizard-next',
-            'lastSelector': '.wizard-last',
-            'onTabShow': function($tab, $navigation, $index) {
-                var $total      = $navigation.find('li').length;
-                var $current    = $index + 1;
-                var $percent    = ($current/$total) * 100;
-
-                // Get vital wizard elements
-                var $wizard     = $navigation.parents('.block');
-                var $progress   = $wizard.find('.wizard-progress > .progress-bar');
-                var $btnPrev    = $wizard.find('.wizard-prev');
-                var $btnNext    = $wizard.find('.wizard-next');
-                var $btnFinish  = $wizard.find('.wizard-finish');
-
-                // Update progress bar if there is one
-                if ($progress) {
-                    $progress.css({ width: $percent + '%' });
-                }
-
-                // If it's the last tab then hide the last button and show the finish instead
-                if($current >= $total) {
-                    $btnNext.hide();
-                    $btnFinish.show();
-                } else {
-                    $btnNext.show();
-                    $btnFinish.hide();
-                }
-
-
-            },
-            'onNext': function($tab, $navigation, $index) {
-                var $valid = $form1.valid();
-
-                if(!$valid) {
-                    $validator1.focusInvalid();
-
-                    return false;
-                }
-            },
-            onTabClick: function($tab, $navigation, $index) {
-                return false;
-            }
-
-
-        });
-    };
 
     // Init wizards with validation, for more examples you can check out http://vadimg.com/twitter-bootstrap-wizard-example/
     var initWizardValidation = function(){
@@ -208,64 +22,6 @@ var BaseFormWizard = function() {
             }
         });
 
-        // Init form validation on classic wizard form
-        var $validator1 = $form1.validate({
-            errorClass: 'help-block animated fadeInDown',
-            errorElement: 'div',
-            errorPlacement: function(error, e) {
-                jQuery(e).parents('.form-group > div').append(error);
-            },
-            highlight: function(e) {
-                jQuery(e).closest('.form-group').removeClass('has-error').addClass('has-error');
-                jQuery(e).closest('.help-block').remove();
-            },
-            success: function(e) {
-                jQuery(e).closest('.form-group').removeClass('has-error');
-                jQuery(e).closest('.help-block').remove();
-            },
-            rules: {
-                'validation-classic-firstname': {
-                    required: true,
-                    minlength: 2
-                },
-                'validation-classic-lastname': {
-                    required: true,
-                    minlength: 2
-                },
-                'validation-classic-email': {
-                    required: true,
-                    email: true
-                },
-                'validation-classic-details': {
-                    required: true,
-                    minlength: 5
-                },
-                'validation-classic-city': {
-                    required: true
-                },
-                'validation-classic-skills': {
-                    required: true
-                },
-                'validation-classic-terms': {
-                    required: true
-                }
-            },
-            messages: {
-                'validation-classic-firstname': {
-                    required: 'Please enter a firstname',
-                    minlength: 'Your firtname must consist of at least 2 characters'
-                },
-                'validation-classic-lastname': {
-                    required: 'Please enter a lastname',
-                    minlength: 'Your lastname must consist of at least 2 characters'
-                },
-                'validation-classic-email': 'Please enter a valid email address',
-                'validation-classic-details': 'Let us know a few thing about yourself',
-                'validation-classic-skills': 'Please select a skill!',
-                'validation-classic-terms': 'You must agree to the service terms!'
-            }
-        });
-
         // Init form validation on the other wizard form
         var $validator2 = $form2.validate({
             errorClass: 'help-block text-right animated fadeInDown',
@@ -282,45 +38,93 @@ var BaseFormWizard = function() {
                 jQuery(e).closest('.help-block').remove();
             },
             rules: {
-                'validation-firstname': {
+                'firstname': {
                     required: true,
+                    lettersonly: true,
                     minlength: 2
                 },
-                'validation-lastname': {
+                'nic': {
                     required: true,
-                    minlength: 2
+                    nic: true
                 },
-                'validation-email': {
+                'personal_email': {
                     required: true,
                     email: true
                 },
-                'validation-details': {
+                'personal_contact': {
                     required: true,
-                    minlength: 5
+                    contact: true,
+                    minlength: 10
                 },
-                'validation-city': {
+                'supplier_category': {
+                    required: true,
+                    valueNotEquals: "default"
+                },
+                'company_name': {
                     required: true
                 },
-                'validation-skills': {
+                'company_email': {
+                    required: true,
+                    email: true
+                },
+                'company_contact': {
+                    required: true,
+                    contact: true,
+                    minlength: 10
+                },
+                'base_city': {
+                    required: true,
+                    valueNotEquals: "default"
+                },
+                'business_address': {
                     required: true
                 },
-                'validation-terms': {
+                'terms_agree': {
                     required: true
                 }
+
             },
             messages: {
-                'validation-firstname': {
+                'firstname': {
                     required: 'Please enter a firstname',
-                    minlength: 'Your firtname must consist of at least 2 characters'
+                    lettersonly: 'Provide a valid firstname',
+                    minlength: 'Your firstname must consist of at least 2 characters'
+
                 },
-                'validation-lastname': {
-                    required: 'Please enter a lastname',
-                    minlength: 'Your lastname must consist of at least 2 characters'
+                'nic': {
+                    required: 'Please enter nic number',
+                    nic: 'Provide a valid nic number'
                 },
-                'validation-email': 'Please enter a valid email address',
-                'validation-details': 'Let us know a few thing about yourself',
-                'validation-skills': 'Please select a skill!',
-                'validation-terms': 'You must agree to the service terms!'
+                'email': 'Please enter a valid email address',
+                'personal_contact': {
+                    required: 'Please enter a contact number',
+                    minlength: 'Provide a valid contact number',
+                    contact: 'Provide a valid contact number'
+                },
+                'supplier_category': {
+                    required: "Please select a category",
+                    valueNotEquals: "Please select a category"
+                },
+                'company_name': {
+                    required: 'Please a enter a company name'
+                },
+                'company_email': 'Please enter a valid email address',
+                'company_contact': {
+                    required: 'Please enter a contact number',
+                    minlength: 'Provide a valid contact number',
+                    contact: 'Provide a valid contact number'
+                },
+                'base_city': {
+                    required: 'Please select a base city',
+                    valueNotEquals: "Please select a base city"
+                },
+                'business_address': {
+                    required: 'Please enter a business address'
+                },
+                'terms_agree': {
+                    required: 'Agree the terms and conditions'
+                }
+
             }
         });
 
@@ -449,6 +253,26 @@ var BaseFormWizard = function() {
         }
     };
 }();
+
+// Custom method to validate letters only.
+jQuery.validator.addMethod("lettersonly", function(value, element) {
+    return this.optional(element) || /^[a-z]+$/i.test(value);
+}, "Letters only please");
+
+// Custom method to validate nic number.
+jQuery.validator.addMethod("nic", function(value, element) {
+    return this.optional(element) || /^[0-9]{9}[vVxX]$/.test(value);
+}, "Valid NIC Number");
+
+// Custom method to validate contact number.
+jQuery.validator.addMethod("contact", function(value, element) {
+    return this.optional(element) || /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/.test(value);
+}, "Valid Contact Number");
+
+// Custom method to validate
+jQuery.validator.addMethod("valueNotEquals", function(value, element, arg){
+    return arg != value;
+}, "Value must not equal arg.");
 
 // Initialize when page loads
 jQuery(function(){ BaseFormWizard.init(); });
